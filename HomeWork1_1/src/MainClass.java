1c3cadf72e50ab128675bf18cc062f505b677af7
RomanZekov <1register1@ukr.net>
2015-10-06 01:25:12 +0300

    maybe final

--- HomeWork1_1/src/MainClass.java
+++ HomeWork1_1/src/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Enter something here : ");
+		System.out.println("Enter a number : ");
 	    
 		   String sInput;
 	     
@@ -21,7 +21,7 @@ public class MainClass {
 	    	   
 	       }
 	       
-	       System.out.println(factorial);
+	       System.out.println("Factorial of the entered number is: " + factorial);
 
 	}
 
