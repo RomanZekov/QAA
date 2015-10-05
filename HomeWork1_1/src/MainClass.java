3e07e2c954de13905349ab64a0ef7a783478ad6d
RomanZekov <1register1@ukr.net>
2015-10-06 01:18:52 +0300

    11

--- HomeWork1_1/src/MainClass.java
+++ HomeWork1_1/src/MainClass.java
@@ -8,11 +8,11 @@ public class MainClass {
 	    
 		   String sInput;
 	     
-	       Scanner scanIn = new Scanner(System.in);
+	       Scanner scanIn = new Scanner( System.in );
 	       sInput = scanIn.nextLine();
 	       scanIn.close();            
 
-	       int value = Integer.parseInt( sInput );
+	       int value = Integer.parseInt(sInput);
 	       int factorial = 1;
 	       
 	       for (int i=1; i <= value; i++) {
