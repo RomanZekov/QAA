c75836d1d064e01eb62e5d1bb9d77cefa2927de1
RomanZekov <1register1@ukr.net>
2015-10-06 01:53:46 +0300

    final

--- HomeWork1_2/src/MainClass.java
+++ HomeWork1_2/src/MainClass.java
@@ -18,10 +18,21 @@ public class MainClass {
 	    int m = Integer.parseInt(sFirst);
 	    int n = Integer.parseInt(sSecond);
 	    
-	    
-	    
-	    System.out.println(sFirst + " : " + sSecond);
-		
+	    if ( ( 10 - m ) == ( 10 - n ) ) {
+	    	
+	    	System.out.println("Both entered numbers close to 10 becouse they are equal");
+	    	
+	    } else 
+	    	if ( ( 10 - m ) > ( 10 - n ) ) {
+	    		
+	    		System.out.println("The closest number to 10 is: " + n);
+	    		
+	    	} else 
+	    		
+	    		{ 
+	    			System.out.println("The closest number to 10 is: " + m); 
+	    		}
+
 	}
 
 }
