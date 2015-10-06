21503289befc63db566b40addf78e13c8e10c1d4
RomanZekov <1register1@ukr.net>
2015-10-06 18:14:19 +0300

    ready

--- HomeWork1_3/src/MainClass.java
+++ HomeWork1_3/src/MainClass.java
@@ -20,15 +20,38 @@ public class MainClass {
 		
 		input.close();
 
-		dis = Math.pow((b*b - 4*a*c), 0.5);
-		System.out.println("Discriminant = "+ dis);
-		 
-		x1 = ((-1)*b+dis)/(2*a); 
-		x2 = ((-1)*b-dis)/(2*a); 
-		 
-		System.out.println("x1 = "+ x1); 
-		System.out.println("x2 = "+ x2);             
+		
+		dis = b * b - 4 * a * c;
+		
+		if ( dis > 0 ) {
+			
+			x1 = ((-1)*b+dis)/(2*a); 
+			x2 = ((-1)*b-dis)/(2*a);
+			
+			System.out.println(" ");
+			System.out.println("x1 = "+ x1); 
+			System.out.println("x2 = "+ x2);   
+			
+		} else {
+			
+			if ( dis == 0 ) {
+				
+				x1 = (-1)*( b / ( 2 * a ) );
+				
+				System.out.println(" ");
+				System.out.println("x1 = x2 = "+ x1);
+				
+			} else {
+				
+				System.out.println(" ");
+				System.out.println("There are no real roots");
 
+			}
+			
+		}
+
+		//System.out.println("Discriminant = "+ dis);
+		 
 	}
 
 }
