62955f7b59b3ae761d4d47c369fc7fa7b5a1848e
RomanZekov <1register1@ukr.net>
2015-10-06 18:37:16 +0300

    ready

---
+++ HomeWork1_4/src/MainClass.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in); 
+		String numStr;
+		
+		System.out.print("Enter a number: " ); 
+		numStr = input.nextLine();
+		input.close();
+		int sum = 0;
+		
+		for (int i = 0; i < numStr.length(); i++) {
+			
+			int x = Integer.parseInt(numStr.substring(i,i+1));
+			
+			sum = sum + x; 
+			
+		}
+		
+		System.out.println("Value is :" + sum );
+
+	}
+
+}
