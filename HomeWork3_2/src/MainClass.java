d0c8c6ea8e0214ac289efe7f3bc2fdc3b04fa518
RomanZekov <1register1@ukr.net>
2015-10-13 08:24:31 +0300

    start

---
+++ HomeWork3_2/src/MainClass.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.TreeSet;
+import java.util.HashSet;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		//Замерить время добавления, поиска и удаления объектов из коллекции в LinkedList, ArrayList, TreeSet, HashSet.
+		
+		LinkedList ll = new LinkedList<>();
+		ArrayList al  = new ArrayList<>();
+		TreeSet ts	  = new TreeSet<>();
+		HashSet hs    = new HashSet<>();
+		
+		
+		
+
+	}
+
+}
