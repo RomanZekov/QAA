da7b4ddc2caf5db494f8e9596ee128d5581e29e8
RomanZekov <1register1@ukr.net>
2015-11-21 20:05:01 +0200

    logger

---
+++ GoogleTest/src/conf/report/Logger.java
@@ -0,0 +1,25 @@
+package conf.report;
+
+import org.testng.Reporter;
+
+/**
+ * Created by User on 20.11.2015.
+ */
+public class Logger {
+
+    public static void log(String message) {
+        Reporter.log(message + "<br>");
+    }
+
+    public static void log(int i) {
+        log("" + i);
+    }
+
+    public static void log(boolean b) {
+        log("" + b);
+    }
+
+    public static void title(String message) {
+        log("<h3>" + message + "</h3>");
+    }
+}
